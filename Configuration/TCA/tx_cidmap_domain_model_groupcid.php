5884584538c5160716db3439f6611da579a3161e
Richard Neumann <r.neumann@homeinfo.de>
2023-04-25 09:06:44 +0200

    Init

---
+++ Configuration/TCA/tx_cidmap_domain_model_groupcid.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    'ctrl' => [
+        'title' => 'Group Customer IDs',
+        'label' => 'feuser_id',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'dividers2tabs' => true,
+        'sortby' => 'sorting',
+        'delete' => 'deleted',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+        ],
+        'searchFields' => 'name,',
+        'iconfile' => 'EXT:pinlogin/Resources/Public/Icons/Extension.svg',
+    ],
+    'types' => [
+        '1' => ['showitem' => 'hidden, feuser_id, pin'],
+    ],
+    'palettes' => [
+        '1' => ['showitem' => ''],
+    ],
+    'columns' => [
+        'hidden' => [
+            'exclude' => 1,
+            'label' => 'Hide',
+            'config' => [
+                'type' => 'check',
+            ],
+        ],
+
+        'feuser_id' => [
+            'exclude' => false,
+            'label' => 'UID',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+
+                'foreign_table' => 'fe_users',
+                'foreign_table_where' => ' ORDER BY username ',
+                
+                'size' => 1,
+                'minitems' => 0,
+            ],
+        ],
+
+        'pin' => [
+            'exclude' => false,
+            'label' => 'PIN',
+            'config' => [
+                'type' => 'user',
+                'renderType' => 'newPINField',
+                'parameters' => [
+                    'size' => '30',
+                    'color' => '#F49700',
+                ],
+                'regularExpression' => [
+                    'expression' => '^[a-zA-Z0-9]{4}$'
+                 ]
+            ],
+        ],
+    ],
+];
---
+++ README.md
@@ -0,0 +1,3 @@
+# typo3-cidmap
+
+[WIP] TYPO3 groups to Customer ID mapping.
