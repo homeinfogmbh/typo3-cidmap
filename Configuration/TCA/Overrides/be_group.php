490db81e018358837eb8f75d49e793ae5afcce8e
Richard Neumann <r.neumann@homeinfo.de>
2023-04-26 15:51:08 +0200

    Update TCA override and translations

--- Configuration/TCA/Overrides/be_group.php
+++ Configuration/TCA/Overrides/be_group.php
@@ -1,4 +1,18 @@
 <?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_group',
+[
+   'tx_examples_options' => [
+      'exclude' => 0,
+      'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:be_group.tx_homeinfo_cid',
+      'config' => [
+         'type' => 'default',
+         'renderType' => 'default'
+      ]
+   ]
+]
+);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
    'be_group',
--- Configuration/TCA/Overrides/fe_group.php
+++ Configuration/TCA/Overrides/fe_group.php
@@ -1,4 +1,18 @@
 <?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_group',
+[
+   'tx_examples_options' => [
+      'exclude' => 0,
+      'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_group.tx_homeinfo_cid',
+      'config' => [
+         'type' => 'default',
+         'renderType' => 'default'
+      ]
+   ]
+]
+);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
    'fe_group',
