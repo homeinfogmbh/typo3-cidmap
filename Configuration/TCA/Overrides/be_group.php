50d212d56eea5e0a3163624464ffdf0b1dcb0ad5
Richard Neumann <r.neumann@homeinfo.de>
2023-04-26 15:52:06 +0200

    Update translation

--- Configuration/TCA/Overrides/be_group.php
+++ Configuration/TCA/Overrides/be_group.php
@@ -5,7 +5,7 @@ defined('TYPO3') or die();
 [
    'tx_examples_options' => [
       'exclude' => 0,
-      'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:be_group.tx_homeinfo_cid',
+      'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:tx_homeinfo_cid',
       'config' => [
          'type' => 'default',
          'renderType' => 'default'
