43573f1e6c9c95608c9c31b7deeccca1563dd04f
Richard Neumann <r.neumann@homeinfo.de>
2023-05-05 10:14:44 +0200

    Add Backend CID mapper

---
+++ Classes/BackendCIDMapper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Homeinfo\cidmap\Controller;
+
+use Generator;
+
+use TYPO3\CMS\Beuser\Domain\Repository\BackendUserGroupRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+
+class BackendCIDMapper
+{
+    private ObjectManager $objectManager;
+    private BackendUserGroupRepository $backendUserGroupRepository;
+
+    function __construct()
+    {
+        $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+        $this->backendUserGroupRepository = $this->objectManager->get(BackendUserGroupRepository::class);
+    }
+
+    public function getCurrentUserCIDs(): Generator
+    {
+        foreach ($this->getCurrentUserGroups() as &$group)
+            yield $group->tx_homeinfo_cid;
+    }
+
+    private function getCurrentUserGroups(): array
+    {
+        $query = $this->backendUserGroupRepository->createQuery();
+        return $query
+            ->where(
+                $queryBuilder->expr()->eq(
+                    'uid',
+                    $queryBuilder->createNamedParameter(Self::getCurrentUserGroupUIDs(), Connection::PARAM_INT_ARRAY)
+                )
+            )->executeQuery()->fetchAll();
+    }
+
+    private static function getCurrentUserGroupUIDs(): array
+    {
+        if (($groupUIDs = Self::getCurrentUser()->userGroupsUID) === NULL)
+            return [];
+
+        return $groupUIDs;
+    }
+
+    private static function getCurrentUser(): array
+    {
+        return $GLOBALS['BE_USER'];
+    }
+}
