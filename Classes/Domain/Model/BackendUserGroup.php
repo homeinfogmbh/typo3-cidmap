68efaa9545ea9eb42f7d8061e5d4f988be4fcb8a
Richard Neumann <r.neumann@homeinfo.de>
2023-05-05 10:51:45 +0200

    Do not pre-set cid field

--- Classes/Domain/Model/BackendUserGroup.php
+++ Classes/Domain/Model/BackendUserGroup.php
@@ -7,5 +7,5 @@ class BackendUserGroup extends \TYPO3\CMS\Extbase\Domain\Model\BackendUserGroup
     /**
      * @var ?int
      */
-    public $tx_homeinfo_cid = NULL;
+    public $tx_homeinfo_cid;
 }
