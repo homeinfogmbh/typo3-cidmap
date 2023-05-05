61592eb1db69dc49fd743fb3272b00226bdb71d1
Richard Neumann <r.neumann@homeinfo.de>
2023-05-05 10:16:52 +0200

    Print current user's CIDs

--- Classes/Controller/DebugController.php
+++ Classes/Controller/DebugController.php
@@ -4,12 +4,14 @@ namespace Homeinfo\cidmap\Controller;
 
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
+use Homeinfo\cidmap\BackendCIDMapper;
+
 class DebugController extends ActionController
 {
     public function debugAction()
     {
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($GLOBALS['BE_USER'], 'BE User:');
-        // Array of all BE user's groups:
-        // $GLOBALS['BE_USER']->userGroupsUID
+        $cidMapper = BackendCIDMapper();
+        $cids = iterator_to_array($cidMapper->getCurrentUserCIDs());
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($cids, "Current user's CIDs:");
     }
 }
