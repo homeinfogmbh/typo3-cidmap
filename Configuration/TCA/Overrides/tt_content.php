ebac3f0ed184d055bb960add8893385f531cf1ee
Richard Neumann <r.neumann@homeinfo.de>
2023-05-11 11:14:25 +0200

    Fix class path

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility\ExtensionUtility::registerPlugin(
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'cidmap',
     'DebugFrontend',
     "View of customer IDs"
